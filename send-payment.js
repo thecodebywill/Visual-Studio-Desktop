61e3cc2f870ab1554bd9d6e27faf4bd29e7953a5
Your Name <you@example.com>
2024-08-11 00:21:36 +0300

    same

--- send-payment.js
+++ send-payment.js
@@ -271,3 +271,21 @@ window.onclick = function(event) {
         }
     }
 };
+
+document.getElementById('connectWalletButton').addEventListener('click', function () {
+    var dropdownMenu = document.querySelector('.dropdown-menu');
+    dropdownMenu.classList.toggle('show');
+});
+
+// Close the dropdown if clicked outside
+window.onclick = function(event) {
+    if (!event.target.matches('#connectWalletButton')) {
+        var dropdowns = document.getElementsByClassName("dropdown-menu");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
